8156bce88c3da2f15cae7aeea74460e6065dadbe
ivankudibal <ivankudibal@users.noreply.github.com>
2019-10-25 18:41:04 +0200

    headless

--- configure-test-suite.sh
+++ configure-test-suite.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ./selenium-accept-infl2
-#sed -i  's/"active": "development"/"active:": "nightly"/' bonitoo.conf.json
+sed -i  's/"headless": false/"headless:": true/' bonitoo.conf.json
 
 echo "INFO: Configuration"
 cat bonitoo.conf.json
