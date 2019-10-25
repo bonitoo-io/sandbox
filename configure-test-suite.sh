2bf98be2fc78c6556269824fb84f55a283638f19
ivankudibal <ivankudibal@users.noreply.github.com>
2019-10-25 14:30:37 +0200

    chore: reorganize into scrtip

--- configure-test-suite.sh
+++ configure-test-suite.sh
@@ -7,3 +7,4 @@ echo "INFO: Cofiguration"
 cat bonitoo.conf.json
 
 
+
---
+++ install-nvm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+
+npm --version # && nvm use 10.16
+npm cache clean -f
+npm install -g n
+npm --version # && nvm use 10.16
+cd selenium-accept-infl2
+sudo /opt/circleci/.nvm/versions/node/v6.1.0/bin/n stable
+npm --version
+which npm
+npm install
+
+
