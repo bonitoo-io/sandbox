60526c28bd70dc09fc4b2ed95b0be75448f0b3d3
ivankudibal <ivankudibal@users.noreply.github.com>
2019-10-25 16:17:18 +0200

    switch to autonomous node, cleanup the host

--- install-nvm.sh
+++ install-nvm.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -x
 
-npm cache clean -f
-
-rm -rf $HOME/.npm/*
+#uninstall 
+rm -rf ~/.nvm
+rm -rf ~/.npm
+rm -rf ~/.bower
+rm -rf $NVM_DIR ~/.npm ~/.bower
+unset NVM_DIR
 rm -rf node*
-sudo apt-get -y remove nodejs
 
+#install what I want
+sudo apt-get -y remove nodejs
 wget https://nodejs.org/dist/v10.16.3/node-v10.16.3-linux-x64.tar.gz
 tar xvfz ./node-v10.16.3-linux-x64.tar.gz
 
