5bd26f3b9e5b8d345c97ba71750d5279578b2f37
ivankudibal <ivankudibal@users.noreply.github.com>
2019-10-25 16:08:14 +0200

    feat from Karl: install of node, npm 10

--- install-nvm.sh
+++ install-nvm.sh
@@ -2,7 +2,10 @@
 
 npm cache clean -f
 
+rm -rf $HOME/.npm/*
 rm -rf node*
+sudo apt-get -y remove nodejs
+
 wget https://nodejs.org/dist/v10.16.3/node-v10.16.3-linux-x64.tar.gz
 tar xvfz ./node-v10.16.3-linux-x64.tar.gz
 
