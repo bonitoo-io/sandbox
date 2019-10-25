a284398880db765950bfeefa7395be9364fff1b1
ivankudibal <ivankudibal@users.noreply.github.com>
2019-10-25 16:41:09 +0200

    b&f

--- install-nvm.sh
+++ install-nvm.sh
@@ -13,6 +13,16 @@ sudo apt-get -y install build-essential checkinstall libssl-dev
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
 
+
+
+export NVM_DIR="/home/circleci/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+nvm use v10.16.3 #and set a default with alias: nvm alias default node.
+nvm alias default node
+
+
+
 #install what I want
 #sudo apt-get -y remove nodejs
 #wget https://nodejs.org/dist/v10.16.3/node-v10.16.3-linux-x64.tar.gz
