d0836a5edc84572a3ef372e82ca707a5e4ac7a73
ivankudibal <ivankudibal@users.noreply.github.com>
2019-10-25 16:37:17 +0200

    rollback to standard installation

--- install-nvm.sh
+++ install-nvm.sh
@@ -8,12 +8,15 @@ rm -rf $NVM_DIR ~/.npm ~/.bower
 unset NVM_DIR NVM_BIN
 rm -rf node*
 
+sudo apt-get update
+sudo apt-get -y install build-essential checkinstall libssl-dev
 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
 
 #install what I want
-sudo apt-get -y remove nodejs
-wget https://nodejs.org/dist/v10.16.3/node-v10.16.3-linux-x64.tar.gz
-tar xvfz ./node-v10.16.3-linux-x64.tar.gz
+#sudo apt-get -y remove nodejs
+#wget https://nodejs.org/dist/v10.16.3/node-v10.16.3-linux-x64.tar.gz
+#tar xvfz ./node-v10.16.3-linux-x64.tar.gz
 
-export PATH=./node-v10.16.3-linux-x64/bin:$PATH
+#export PATH=./node-v10.16.3-linux-x64/bin:$PATH
 
