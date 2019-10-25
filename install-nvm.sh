20aaa9b0d8b45b245dcd4e18bb40671bd4f37423
ivankudibal <ivankudibal@users.noreply.github.com>
2019-10-25 16:33:47 +0200

    fix: add node to PATH

--- install-nvm.sh
+++ install-nvm.sh
@@ -5,11 +5,15 @@ rm -rf ~/.nvm
 rm -rf ~/.npm
 rm -rf ~/.bower
 rm -rf $NVM_DIR ~/.npm ~/.bower
-unset NVM_DIR
+unset NVM_DIR NVM_BIN
 rm -rf node*
+
+
 
 #install what I want
 sudo apt-get -y remove nodejs
 wget https://nodejs.org/dist/v10.16.3/node-v10.16.3-linux-x64.tar.gz
 tar xvfz ./node-v10.16.3-linux-x64.tar.gz
+
+export PATH=./node-v10.16.3-linux-x64/bin:$PATH
 
