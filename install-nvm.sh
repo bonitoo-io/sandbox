13fbe94212df957fcf85cd4d7e077a69a5c58b82
ivankudibal <ivankudibal@users.noreply.github.com>
2019-10-25 15:09:32 +0200

    right version?

--- install-nvm.sh
+++ install-nvm.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 
-
-rm -rf node-v12.13.0-linux-x64
-wget https://nodejs.org/dist/v12.13.0/node-v12.13.0-linux-x64.tar.gz
-tar xvfz  ./node-v12.13.0-linux-x64.tar.gz
+rm -rf node*
+wget https://nodejs.org/dist/v10.16.3/node-v10.16.3-linux-x64.tar.gz
+tar xvfz ./node-v10.16.3-linux-x64.tar.gz
 
