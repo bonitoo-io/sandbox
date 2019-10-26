cd6591dbd00bca10efb8608cd2da26e158b676e1
ivankudibal <ivankudibal@users.noreply.github.com>
2019-10-26 14:59:25 +0200

    fix sed

--- configure-test-suite.sh
+++ configure-test-suite.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 cd ./selenium-accept-infl2
-sed -i  's/"headless": false/"headless:": true/' bonitoo.conf.json
-sed -i  's/"host": "aws-somewhere"/"host": "localhost"' bonitoo.conf.json
-sed -i 's/setTimeout(resolve, timeout);/setTimeout(resolve, 12000);/' ./src/pages/basePage.js
+sed -i 's/"headless": false/"headless:": true/' bonitoo.conf.json
+sed -i 's/"host": "aws-somewhere"/"host": "localhost"/' bonitoo.conf.json
+sed -i 's/resolve, timeout/resolve, 12000/' ./src/pages/basePage.js
 
 echo "INFO: Configuration"
 cat bonitoo.conf.json
 
+echo "INFO: Code"
+cat ./src/pages/basePage |grep 12000
 
 
