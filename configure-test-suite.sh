854e76bca3abc77237780a2eef08d08e674920f2
ivankudibal <ivankudibal@users.noreply.github.com>
2019-10-26 12:08:56 +0200

    add check if docker is running

--- configure-test-suite.sh
+++ configure-test-suite.sh
@@ -2,6 +2,7 @@
 
 cd ./selenium-accept-infl2
 sed -i  's/"headless": false/"headless:": true/' bonitoo.conf.json
+sed -i  's/"host": "aws-somewhere"/"host": "localhost"' bonitoo.conf.json
 
 echo "INFO: Configuration"
 cat bonitoo.conf.json
--- run-test.sh
+++ run-test.sh
@@ -2,7 +2,7 @@
 
 sudo docker run -p9999:9999 --name influx-v2 quay.io/influxdb/influx:nightly /entrypoint.sh influxd --e2e-testing=true &
 sleep 30
-
+curl http://localhost:9999 || return $?
 
 source ./env
 
