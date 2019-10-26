5bee577af46e8bfd5c990da58df64f060f505965
ivankudibal <ivankudibal@users.noreply.github.com>
2019-10-26 14:31:36 +0200

    check

--- run-test.sh
+++ run-test.sh
@@ -1,17 +1,20 @@
-#!/bin/bash  -x
+#!/bin/bash
 
 echo "DEBUG: Run docker"
 docker run -p9999:9999 --net host --name influx-v2 quay.io/influxdb/influx:nightly /entrypoint.sh influxd --e2e-testing=true &
-
-if ! ./check-if-running.sh ; then
-    echo "DEBUG: influx did not start"
-    exit 1
-fi
 
 source ./env
 
 echo "DEBUG: install node_modules"
 npm i
+
+
+echo "DEBUG: make sure influx-v2 has started"
+if ! ./check-if-running.sh ; then
+    echo "DEBUG: influx did not start"
+    exit 1
+fi
+
 echo "DEBUG: run test"
 npm test
 
