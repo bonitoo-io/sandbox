5155c6d232b6c326040972269b454eed19561ab0
ivankudibal <ivankudibal@users.noreply.github.com>
2019-10-26 14:37:06 +0200

    reorganize to run docker right before test

--- run-test.sh
+++ run-test.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo "DEBUG: Run docker"
-docker run -p9999:9999 --net host --name influx-v2 quay.io/influxdb/influx:nightly /entrypoint.sh influxd --e2e-testing=true &
-
 source ./env
 
 echo "DEBUG: install node_modules"
 npm i
 
+echo "DEBUG: Run docker"
+docker run -p9999:9999 --net host --name influx-v2 quay.io/influxdb/influx:nightly /entrypoint.sh influxd --e2e-testing=true &
 
 echo "DEBUG: make sure influx-v2 has started"
-if ! ./check-if-running.sh ; then
+if ! ../check-if-running.sh ; then
     echo "DEBUG: influx did not start"
     exit 1
 fi
+
 
 echo "DEBUG: run test"
 npm test
