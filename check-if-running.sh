6936139936efd4d5d3972347d130372c8bb32ace
karel-rehor <karel.k.rehor@email.cz>
2019-10-29 17:36:02 +0100

    restore polling loop

--- check-if-running.sh
+++ check-if-running.sh
@@ -8,19 +8,19 @@ ping -c 4 localhost
 
 sudo netstat -tlnp 
 
-#while [ $n -lt $MMAX ]
-#do
+while [ $n -lt $MMAX ]
+do
     if curl -v http://localhost:9999/ --connect-timeout 60  --max-time 60 ; then
         echo "Influx is running"
  #       break;
-    else
-        echo "failed to connect to Influx at localhost:9999"
-        exit 1
+#    else
+#        echo "failed to connect to Influx at localhost:9999"
+#        exit 1
     fi
-#    sleep $WAITME
-#    n=$[n+1]
-#done
+    sleep $WAITME
+    n=$[n+1]
+done
 
-#if [ $n -eq $MMAX ]; then 
-#    exit 1
-#fi
+if [ $n -eq $MMAX ]; then 
+    exit 1
+fi
