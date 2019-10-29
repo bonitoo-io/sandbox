a53143b672a6fe7803563c2b1ee0b359d46722cd
karel-rehor <karel.k.rehor@email.cz>
2019-10-29 14:01:37 +0100

    troubleshoot with enforced wait

--- run-test.sh
+++ run-test.sh
@@ -8,6 +8,9 @@ npm i
 echo "DEBUG: Run docker"
 sudo docker run -d -p 9999:9999 --net host -v /var/run/docker.sock:/var/run/docker.sock --name influx-v2 quay.io/influxdb/influx:nightly /entrypoint.sh influxd --e2e-testing=true
 
+echo "sleeping 30 seconds"
+sleep 30
+
 echo "DEBUG: make sure influx-v2 has started"
 if ! ../check-if-running.sh ; then
     echo "DEBUG: influx did not start"
