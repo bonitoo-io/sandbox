128a8c31ce39a43146b747966a58684af4c0b213
karel-rehor <karel.k.rehor@email.cz>
2019-10-29 13:52:15 +0100

    simplify check-if-running.sh

--- check-if-running.sh
+++ check-if-running.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 n=0
-MMAX=15
+MMAX=60
 WAITME=3
 
 sudo less /etc/hosts
@@ -10,16 +10,19 @@ ping -c 4 localhost
 
 sudo netstat -tlnp 
 
-while [ $n -lt $MMAX ]
-do
-    if curl -v http://localhost:9999/ ; then
+#while [ $n -lt $MMAX ]
+#do
+    if curl -v http://localhost:9999/ --connect-timeout 60  --max-time 60 ; then
         echo "Influx is running"
-        break;
+ #       break;
+    else
+        echo "failed to connect to Influx at localhost:9999"
+        exit 1
     fi
-    sleep $WAITME
-    n=$[n+1]
-done
+#    sleep $WAITME
+#    n=$[n+1]
+#done
 
-if [ $n -eq $MMAX ]; then 
-    exit 1
-fi
+#if [ $n -eq $MMAX ]; then 
+#    exit 1
+#fi
